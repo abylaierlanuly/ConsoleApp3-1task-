f27b89c1c0216f45c1c5b9e3dd86101c9a60e6b3
Abyl <Abyl@DESKTOP-2SC6383>
2020-04-30 16:43:09 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp3(1task)/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp3_1task_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            School sh = new School(2);
+            Console.WriteLine(sh);
+        }
+    }
+
+    class School {
+        List<School> schools = new List<School>();
+        int _number;
+        public School(int number) {
+            _number = number;
+        }
+
+        public void AddClass(School schools) {
+            schools.AddClass(schools);
+        }
+     
+    }
+
+    class Claas {
+        List<Students> students = new List<Students>();
+        string _claasname;
+        public Claas(string claasname) {
+            _claasname = claasname;
+        }
+        public void AddStudents(Students student) {
+            students.Add(student); 
+        }
+    
+    }
+
+    class Students {
+        string _studentname;
+        public Students(String studentname) {
+            _studentname = studentname;
+        }
+    }
+
+
+
+}
